fee98eb8d7d3ca098d9555e64b74b5a9eaf19a3e
Rick saha <ricksaha2002@gmail.com>
2025-08-05 01:07:07 +0530

    some change in backend code

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
 
-const allowedOrigins = [process.env.FRONTEND_URL];
+// const allowedOrigins = [process.env.FRONTEND_URL];
+// origin: allowedOrigins,
 
 app.use(
   cors({
-    // origin: "https://rd-frontend.onrender.com",
-    origin: allowedOrigins,
+    origin: "https://rd-frontend-tau.vercel.app/",
     credentials: true,
   })
 );
