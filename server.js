e9a829d0c8356216afffcab58c709bf0fa48c0ff
Rick saha <ricksaha2002@gmail.com>
2025-08-05 01:12:25 +0530

    some change in backend code

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://rd-frontend-tau.vercel.app/",
+    origin: "https://rd-frontend-tau.vercel.app",
     credentials: true,
   })
 );
